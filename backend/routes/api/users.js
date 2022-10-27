bb995702b407cd87282c728dfa6c8f9ac6ae5624
Joonil Kim <bartholomaeuskim@gmail.com>
2022-10-27 11:17:20 -0700

    Something modified

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -35,8 +35,8 @@ const validateSignup = [
 const router = express.Router()
 
 router.post('/', validateSignup, async (req, res) => {
-    const { email, password, username } = req.body
-    const user = await User.signup({ email, username, password })
+    const { email, password, username, firstName, lastName } = req.body
+    const user = await User.signup({ email, username, password, firstName, lastName })
 
     setTokenCookie(res, user)
 
