1e426fecc69934ccd31147aea9b6f7f987e4cb0e
Joonil Kim <bartholomaeuskim@gmail.com>
2022-10-27 21:59:16 -0700

    debugging

--- backend/db/models/group.js
+++ backend/db/models/group.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Group.belongsTo(models.User, { as: 'Organizer' })
+      Group.belongsTo(models.User, { as: 'Organizer', foreignKey: 'organizerId' })
 
       Group.belongsToMany(
         models.User,
