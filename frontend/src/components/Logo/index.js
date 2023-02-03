376514c8317c1fa2363b07ad000e496304534d10
Joonil Kim <hydralisk.ks@gmail.com>
2023-02-03 03:06:58 -0700

    seed

--- frontend/src/components/Logo/index.js
+++ frontend/src/components/Logo/index.js
@@ -1,11 +1,10 @@
-import { Link } from 'react-router-dom'
 import logoImage from '../../assets/logo.svg'
 import styles from './logo.module.css'
 
 const Logo = () => {
     return (
         <div className={styles.logo}>
-            <img src={logoImage} alt='logo' />
+            <img src={logoImage} width='150' height='34' alt='logo' />
         </div>
     )
 }
